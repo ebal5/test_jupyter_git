0e97db5196848c2b1e2bb40d28dbb0ee3f0f94bd
ebal5 <6240240+ebal5@users.noreply.github.com>
2024-03-25 17:06:58 +0000

    fix src

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,6 +4,6 @@ from sklearn.datasets import load_iris
 def load_iris_data():
     iris = load_iris()
     df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-    df['target'] = iris.target
+    df['target'] = iris.target.astype(int)
     df['target_names'] = df['target'].apply(lambda x: iris.target_names[x])
     return df
--- src/visualizations.py
+++ src/visualizations.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 def plot_pairplot(df):
     sns.pairplot(df, hue='target_names')
     plt.show()
 
 def plot_heatmap(df):
-    sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+    numeric_columns = df.select_dtypes(include=[np.number]).columns
+    sns.heatmap(df[numeric_columns].corr(), annot=True, cmap='coolwarm')
     plt.show()
